215ebbe5f5ec9bbd7e1e554efd7b86af2b034aba
AlexFrancis95 <alexfrancis@email.com>
2019-01-21 10:18:58 +0000

    added navbar + linked scss

--- src/app.js
+++ src/app.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import './scss/style.scss';
+
+import Header from './components/Header';
+
 class App extends React.Component {
 
   render() {
     return(
       <div>
+        <Header />
         <h1>Hello World</h1>
       </div>
     );
---
+++ src/components/header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class Header extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+
+    return (
+      <header>
+        <nav>
+          <div className="nav-left">
+            <ul>
+              <li><img className="logo" src="logo.png" alt=""/></li>
+              <li className="green">Explore</li>
+              <li>News</li>
+              <li>Top Setlists</li>
+            </ul>
+          </div>
+          <div className="nav-right">
+            <ul>
+              <li>Profile</li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default Header;
